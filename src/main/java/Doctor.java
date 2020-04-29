aa2ccf892074c7170657f10309cf7f10cb309b1f
AlejandroManzano <alex.batt99@gmail.com>
2020-04-28 22:41:01 -0500

    Se agrega una clase anidad, se explica el foreach para una coleccion

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
 
     static int id;
@@ -28,5 +31,49 @@ public class Doctor {
                 "nombre='" + nombre + '\'' +
                 ", especialidad='" + especialidad + '\'' +
                 '}';
+    }
+
+    ArrayList<CitaDisponible> citasDisponibles = new ArrayList<CitaDisponible>();
+    public void generaCitaDisponible(Date fecha, String hora){
+        citasDisponibles.add(new CitaDisponible(fecha, hora));
+    }
+
+    public ArrayList<CitaDisponible>  obtenCitaGenerada(){
+        return citasDisponibles;
+    }
+
+    public static class CitaDisponible{
+        private int id;
+        private Date fecha;
+        private String hora;
+
+        public CitaDisponible(Date fecha, String hora) {
+            this.fecha = fecha;
+            this.hora = hora;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getFecha() {
+            return fecha;
+        }
+
+        public void setFecha(Date fecha) {
+            this.fecha = fecha;
+        }
+
+        public String getHora() {
+            return hora;
+        }
+
+        public void setHora(String hora) {
+            this.hora = hora;
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import static UI.UIMenu.*;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -11,6 +12,17 @@ public class Main {
         //Ejemplo de sobrecarga de metodo constructor
         Doctor doctor = new Doctor("Alejandro","Nefrologo");
         System.out.println(doctor.toString());
+
+        doctor.generaCitaDisponible(new Date(), "4pm");
+        doctor.generaCitaDisponible(new Date(), "5pm");
+
+        //Para el foreach dentro de un arrayList, instanciar objeto que se llene, este sera de la clase del arrayList
+        //:
+        //invocar el metodo con el cual se recorra ese objeto o se podra obtener un get
+        for (Doctor.CitaDisponible citasDisponibles:
+             doctor.obtenCitaGenerada()) {
+            System.out.println("Cita generada: " + citasDisponibles.getFecha() + " " + citasDisponibles.getHora());
+        }
 
         Paciente paciente = new Paciente("Alejandro", "a@email.com");
         //paciente.peso = 10; //Esta asignación falla debido a que se le coloco un modificador de acceso al campo asociado
